ad236191ffc4d1a525dcbe5023fa997264b9d1da
Ludovic Vanasse <ludovicvanasse@gmail.com>
2021-12-21 01:27:24 -0500

    Initial setup for Zephyr

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TamagotCharles
+
+## Build
+- west init
+- west update
+- west build -b nucleo_f401re
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <zephyr.h>
+#include <sys/printk.h>
+
+void main(void) {
+
+  printk("The TamagotCharles is a handheld digital Charles that was created in "
+         "Longueuil by Ludovic Vanasse out of boredom.\n");
+}
